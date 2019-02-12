e8e0dc3c03b71e1b02bb9951cc943c84bedc4b27
Mark Oliver <mark.c.oliver@gmail.com>
2019-02-11 19:51:46 -0500

    update README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# lambda_data
+DS-Unit-3-Sprint-1-Software-Engineering module1-python-modules-packages-and-environments
+
+
--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdadata_invegat2",
-    version="0.0.6",
+    version="0.0.7",
     author="invegat",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/invegat/lambdadata",
+    url="https://github.com/invegat/lambda_data",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires=REQUIRED,
